c084ecfb3aabd72ed3d74c38debaffb302e0bb1c
Gabby <glsbutterfield@gmail.com>
2024-10-26 17:41:50 -0600

    calculated amountDue *100 from purchase price and money tendered

--- src/makechange/CashRegister.java
+++ src/makechange/CashRegister.java
@@ -4,19 +4,28 @@ public class CashRegister {
 	public static void main(String[] args) {
 		java.util.Scanner sc = new java.util.Scanner(System.in);
 		System.out.println("What is the amount of your purchase? ");
-		double amount = sc.nextDouble();
+		double purchase = sc.nextDouble();
 		
 		System.out.println("What is the amount of money provided? ");
 		double tendered = sc.nextDouble();
 		
-		if (amount > tendered) {
+		int amountDue, twentiesDue, tensDue, fivesDue, onesDue, quartersDue, dimesDue, nickelsDue, penniesDue; 
+		
+		if (purchase > tendered) {
 			System.out.println("Sorry, that's not enough to cover your purchase.");
 		}
 		
-		else if (amount == tendered) {
+		else if (purchase == tendered) {
 			System.out.println("You paid the exact amount of your purchase. Have a great day.");
 		}
 		
-		
+		else {
+			amountDue = (int) ((tendered - purchase) *100);
+			System.out.println(amountDue);
+//			if (amountDue % 100 == 0) {
+				
+//			}
+			
+		}
 	}
 }
